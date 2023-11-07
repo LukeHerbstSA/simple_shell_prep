90cc6df3f55ae2e1ba78e58f5ec3828bea89bed5
Luke Herbst <lukeherbst37@gmail.com>
2023-11-07 15:28:10 +0200

    added wokring solution to executute standard functions like ls from files using execve

--- execve_shell_cmd.c
+++ execve_shell_cmd.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-	const char cmd_path[] = "/usr/bin/ls";
-	const char *args_array[] = {"ls", NULL};
+	const char cmd_path[] = "C:/Program Files/Git/usr/bin/ls";
+	const char *args_array[] = {"ls", "-l", NULL};
 	const char *env_vars[] = {NULL};
 
 	if (execve(cmd_path, args_array, env_vars) == -1);
